f1f8d436fb39ce45651f55c01a2b82131921a1fd
Avinash Chitare <chitareavinash6@gmail.com>
2025-06-06 01:34:04 +0530

    Fix issue while deploying

--- admin/src/pages/Doctor/DoctorDashBoard.jsx
+++ admin/src/pages/Doctor/DoctorDashBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { DoctorContext } from "../../context/DoctorContext";
-import { get } from "mongoose";
+
 import { assets } from "../../assets/assets_admin/assets";
 import { AppContext } from "../../context/AppContext";
 const DoctorDashBoard = () => {
